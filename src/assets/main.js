ad9818b1964fe2b0e4c9770506ba99f0ff54ca44
jarrattisted <jarratt@helpdocs.io>
2016-12-12 06:19:15 +0000

    build completed

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,24 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/jakeapeters.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      console.log(response);
+
+      // Convert completed courses array length into number
+      var completedLength = response.courses.completed.length
+
+      // Count up as long as it is less than completedLength
+      for (var i = 0; i < completedLength; i++) {
+        var courseTitle = response.courses.completed[i].title;
+        var courseImage = response.courses.completed[i].badge;
+        var courseUrl = response.courses.completed[i].url;
+
+        $("#badges").append("<div class='course'><h3>" + courseTitle  + "</h3><img src=" + courseImage + "><a href=" + courseUrl + " target='_blank' style='btn btn-primary'>See Course</a></div>");
+      }
+
+    }
+  });
 
 });
