a16029cea6dcd8e18201d6614bee9a457eda9ea6
jarrattisted <jarratt@helpdocs.io>
2016-12-12 06:21:23 +0000

    added bootstrap style buttons

--- src/assets/main.js
+++ src/assets/main.js
@@ -15,7 +15,7 @@ $(function() {
         var courseImage = response.courses.completed[i].badge;
         var courseUrl = response.courses.completed[i].url;
 
-        $("#badges").append("<div class='course'><h3>" + courseTitle  + "</h3><img src=" + courseImage + "><a href=" + courseUrl + " target='_blank' style='btn btn-primary'>See Course</a></div>");
+        $("#badges").append("<div class='course'><h3>" + courseTitle  + "</h3><img src=" + courseImage + "><a href=" + courseUrl + " target='_blank' class='btn btn-primary'>See Course</a></div>");
       }
 
     }
